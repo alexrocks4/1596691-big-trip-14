e66cc6c7665419166ecf0600832b6f9ff7da4f19
alex <sakun.alexey@gmail.com>
2021-05-24 13:05:48 +0300

    Fix B18

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,18 +1,5 @@
 import { MINUTES_IN_A_DAY, MINUTES_IN_A_HOUR } from './const.js';
 
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#getting_a_random_integer_between_two_values
-const getRandomIntegerInclusive = (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
-};
-
-const getRandomArrayElement = (elements) => {
-  const randomArrayIndex = getRandomIntegerInclusive(0, elements.length - 1);
-
-  return elements[randomArrayIndex];
-};
-
 const isEscKeyPressed = (evt) => {
   return evt.key === 'Escape' || evt.key === 'Esc';
 };
@@ -52,8 +39,6 @@ const isOnline = () => {
 };
 
 export {
-  getRandomIntegerInclusive,
-  getRandomArrayElement,
   isEscKeyPressed,
   setToInteger,
   formatDuration,
